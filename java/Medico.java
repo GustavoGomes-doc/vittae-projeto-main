4f3f3e1fd1b1aef87673394189f4055285a41638
Seu Nome Aqui <seu.email@exemplo.com>
2025-11-07 20:02:45 -0300

    first commit

---
+++ java/Medico.java
@@ -0,0 +1,69 @@
+import java.util.Date;
+
+public  class Medico {
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String crm;
+    private Date data_nasc;
+    private String telefone;
+    private String especialidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public Date getData_nasc() {
+        return data_nasc;
+    }
+
+    public void setData_nasc(Date data_nasc) {
+        this.data_nasc = data_nasc;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    
+}
---
+++ java/Paciente.java
@@ -0,0 +1,59 @@
+
+import java.util.Date;
+
+public class Paciente {
+    private String nome;
+    private String cpf;
+    private String rg;
+    private String telefone;
+    private String email;
+    private Date data_nasc;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getData_nasc() {
+        return data_nasc;
+    }
+
+    public void setData_nasc(Date data_nasc) {
+        this.data_nasc = data_nasc;
+    }
+}
